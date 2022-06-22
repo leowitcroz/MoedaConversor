c0e5f0ca7b07c8d57ecd4913a51a6375eca75c9e
Leonardo <leowitcroz@gmail.com>
2022-06-22 20:02:38 -0300

    first commit

---
+++ src/app/componentes/redirecionamento/redirecionamento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-redirecionamento',
+  templateUrl: './redirecionamento.component.html',
+  styleUrls: ['./redirecionamento.component.scss']
+})
+export class RedirecionamentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
