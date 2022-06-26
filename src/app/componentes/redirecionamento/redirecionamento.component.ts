8fc6257386a031945890fa7d62d8491448f3f091
Leonardo <leowitcroz@gmail.com>
2022-06-26 11:20:30 -0300

    media para mobile feita

--- src/app/componentes/redirecionamento/redirecionamento.component.ts
+++ src/app/componentes/redirecionamento/redirecionamento.component.ts
@@ -3,16 +3,12 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-redirecionamento',
   templateUrl: './redirecionamento.component.html',
-  styleUrls: ['./redirecionamento.component.scss']
+  styleUrls: ['./redirecionamento.component.scss'],
 })
 export class RedirecionamentoComponent implements OnInit {
-
-  
-
   constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   realdollar: any;
   realiene: any;
@@ -25,61 +21,36 @@ export class RedirecionamentoComponent implements OnInit {
   convertereallibra: number | undefined;
   converterealpeso: number | undefined;
 
+  converterrealdollar(): any {
+    console.log(this.realdollar);
+    let real = this.realdollar;
+    this.converterealdolar = Math.round(real / 5.3);
 
-  converterrealdollar(): any{
+    return this.converterealdolar;
+  }
 
-      console.log(this.realdollar)
-      let real = this.realdollar;
-      this.converterealdolar = Math.round(real / 5.30);
-      
+  converterrealiene(): any {
+    let real2 = this.realiene;
+    this.converterealiene = Math.round(real2 * 25.78);
+    return this.converterealiene;
+  }
 
-      return this.converterealdolar;
+  converterrealeuro(): any {
+    let real3 = this.realeuro;
+    this.converterealeuro = Math.round(real3 / 5.54);
+    return this.converterealeuro;
+  }
 
-      
-    }
+  converterreallibra(): any {
+    let real4 = this.reallibra;
+    this.convertereallibra = Math.round(real4 / 6.43);
 
+    return this.convertereallibra;
+  }
 
-    converterrealiene(): any{
-
-      let real2 = this.realiene;
-      this.converterealiene =  Math.round(real2 * 25.78);
-      return this.converterealiene;
-
-      
-    }
-
-
-    
-    converterrealeuro(): any{
-
-      let real3 = this.realeuro;
-      this.converterealeuro =  Math.round(real3 / 5.54);
-      return this.converterealeuro;
-
-      
-    }
-
-
-    converterreallibra(): any{
-
-      let real4 = this.reallibra;
-      this.convertereallibra =  Math.round(real4 / 6.43);
-      
-      return this.convertereallibra;
-
-      
-    }
-
-
-
-    converterrealpeso(): any{
-
-      let real5 = this.realpeso;
-      this.converterealpeso = Math.round (real5 * 23.70);
-      return this.converterealpeso;
-
-      
-    }
-
- 
+  converterrealpeso(): any {
+    let real5 = this.realpeso;
+    this.converterealpeso = Math.round(real5 * 23.7);
+    return this.converterealpeso;
+  }
 }
